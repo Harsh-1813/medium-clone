5a51c41bbb0cc9ca0b093b0db2a9348e32830aa8
Harsh-1813 <harsh.gupta1813@gmail.com>
2025-07-29 21:49:51 +0530

    frontend/signup and signin page

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,72 @@
+import type { SignupInput } from "@noisytech/medium-common";
+import { useState, type ChangeEvent } from "react";
+import { Link } from "react-router-dom"
+
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+    const [postInputs, setPostInputs] = useState<SignupInput>({
+        name: "",
+        email: "",
+        password: ""
+    });
+    return <div className="h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div>
+                <div className="px-10">
+                    <div className="text-3xl text-center font-extrabold">
+                        Create an Account
+                    </div>
+                    <div className="text-slate-400 text-center">
+                        {type === "signin" ? "Don't have an account?" : "Already have an account?"}
+                        <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>{type === "signin" ? "Create Account" : "Login"}</Link>
+                    </div>
+                </div>
+                <div className="pt-8">
+                    {type === "signup" ?
+                        <LabelledInput label="Name" placeholder="Enter your name" onChange={(e) => {
+                            setPostInputs({
+                                ...postInputs,
+                                name: e.target.value
+                            })
+                        }} />
+                        : <></>}
+                    <LabelledInput label="Email" placeholder="abc@gmail.com" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            email: e.target.value
+                        })
+                    }} />
+                    <LabelledInput label="Password" type={"password"} placeholder="" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            name: e.target.value
+                        })
+                    }} />
+                </div>
+                <div className="pt-8">
+                    <button className="bg-black w-full hover:bg-gray-500 text-white font-semibold py-2 px-4 border border-blue-700 rounded-lg">
+                        {type === "signin" ? "Sign In" : "Sign Up"}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div >
+}
+
+interface LabelledInputType {
+    label: string;
+    placeholder: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+}
+
+function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
+    return <div>
+        <div>
+            <form>
+                <label className="block mb-2 text-sm font-semibold text-black pt-4">{label}</label>
+                <input onChange={onChange} type={type || "text"} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder={placeholder} required />
+            </form>
+        </div>
+    </div>
+}
---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-2xl font-bold">
+                    "The customer support I recieved was exceptional. The support team went above and beyond to
+                    address my concerns."
+                </div >
+                <div className="max-w-md text-lg font-semibold text-left mt-4">
+                    Julies Winfield
+                </div>
+                <div className="max-w-md text-sm font-semibold text-left text-slate-400">
+                    CEO, Acme Inc
+                </div>
+            </div>
+        </div>
+    </div>
+}
